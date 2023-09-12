99c840cd004c526c8f5b719a4da449f1035af193
Jhoel Bagnasco <bagnascojhoel@gmail.com>
2023-09-11 22:56:37 -0300

    feat: adapt 404 page to my style

--- blog/components/Page404.tsx
+++ blog/components/Page404.tsx
@@ -1,31 +1,30 @@
 import * as React from 'react'
+import { useNotionContext } from 'react-notion-x'
+import cs from 'classnames';
 
 import * as types from '@/lib/types'
+import {rootNotionPageId} from '@/lib/config'
 
+
+import { NotionPageHeader } from './NotionPageHeader'
 import { PageHead } from './PageHead'
 import styles from './styles.module.css'
 
-export const Page404: React.FC<types.PageProps> = ({ site, pageId, error }) => {
-  const title = site?.name || 'Notion Page Not Found'
+export const Page404: React.FC<types.PageProps> = ({ site, pageId }) => {
+  const title = site?.name || 'Page Not Found'
+  const { components, mapPageUrl } = useNotionContext();
 
   return (
     <>
       <PageHead site={site} title={title} />
+      <NotionPageHeader/>
 
       <div className={styles.container}>
         <main className={styles.main}>
-          <h1>Notion Page Not Found</h1>
-
-          {error ? (
-            <p>{error.message}</p>
-          ) : (
-            pageId && (
-              <p>
-                Make sure that Notion page &quot;{pageId}&quot; is publicly
-                accessible.
-              </p>
-            )
-          )}
+          <h1 className={cs('notion-h-title', 'text-color')}>Notion Page Not Found</h1>
+          <p className={cs('text-color')}>
+            This page is not available, <a href="/" className={cs('notion-link')}> click here to go home.</a>
+          </p>
 
           <img
             src='/404.png'
