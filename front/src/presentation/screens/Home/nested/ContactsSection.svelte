32f0ffb27cfbca62bdb93b113958808921666221
Jhoel Bagnasco <bagnascojhoel@gmail.com>
2023-10-24 08:34:21 -0300

    style(front): fix contact section on mobile

--- front/src/presentation/screens/Home/nested/ContactsSection.svelte
+++ front/src/presentation/screens/Home/nested/ContactsSection.svelte
@@ -21,8 +21,8 @@
     }
 </script>
 
-<section class="mt-24 lg:mt-0">
-    <ul class="mt-7 flex flex-row justify-center gap-4">
+<section class="mt-1 md:mt-24 lg:mt-0">
+    <ul class="mt-7 flex flex-col md:flex-row justify-center gap-4">
         <li>
             <a
                 href={PERSONAL_LINKEDIN_URL}
